0da94a33b7e16cec7f23b792624d71902172c0f9
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-03-09 23:05:41 -0700

    Update initialize-pipeline.ps1

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -4,7 +4,7 @@ Write-Host "Build reason: $buildReason"
 if ($buildReason -eq "PullRequest") {
   # parse PR title to see if we should pack this
   $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
-  $env:PULLREQUEST_TITLE = $response.title.ToLowerInvariant()
-  $title =  $env:PULLREQUEST_TITLE
+  $title = $response.title.ToLowerInvariant()
+  "##vso[task.setvariable variable=PullRequestTitle]$title"
   Write-Host "Title: $title"
 }
