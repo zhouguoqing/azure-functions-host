5c8b63b63deb1887f30ea835e4721524cb4fd14a
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-03-16 13:41:21 -0700

    Update initialize-pipeline.ps1

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -1,20 +1,20 @@
 $buildReason = $env:BUILD_REASON
 $sourceBranch = $env:BUILD_SOURCEBRANCH
-$bypassPackaging = "true"
-$includeSuffix = "true"
+$bypassPackaging = $true
+$includeSuffix = $true
 Write-Host "SourceBranch: $sourceBranch, Build reason: $buildReason"
 
 if($sourceBranch.endsWith('devops_experiment') -and ($buildReason -ne "PullRequest"))
 {
-  $includeSuffix = "false"
-  $bypassPackaging = "false"
+  $includeSuffix = $false
+  $bypassPackaging = $false
 }
 elseif($buildReason -eq "PullRequest")
 {
   $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
   $title = $response.title.ToLowerInvariant()
   if ($title.Contains("[pack]")) {
-    $bypassPackaging = "false"
+    $bypassPackaging = $false
   }
 }
 
