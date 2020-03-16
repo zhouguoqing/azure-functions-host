7fbfbb83d9b07e9ebcbac720bae0e5f6239cb03e
Yogesh Jagadeesan <yojagad@microsoft.com>
2020-03-16 12:15:29 -0700

    Update initialize-pipeline.ps1

--- build/initialize-pipeline.ps1
+++ build/initialize-pipeline.ps1
@@ -1,12 +1,23 @@
 $buildReason = $env:BUILD_REASON
-$title = "N/A"
-Write-Host "Build reason: $buildReason"
-"##vso[task.setvariable variable=PullRequestTitle]$title"
+$sourceBranch = $env:BUILD_SOURCEBRANCH
+$bypassPackaging = $true
+$includeSuffix = 1
+Write-Host "SourceBranch: $sourceBranch, Build reason: $buildReason"
 
-if ($buildReason -eq "PullRequest") {
-  # parse PR title to see if we should pack this
+if($sourceBranch.endsWith('master') -and ($buildReason -ne "PullRequest"))
+{
+  $includeSuffix = 0 #Setting to false
+  $bypassPackaging = $false
+}
+elseif($buildReason -eq "PullRequest")
+{
   $response = Invoke-RestMethod api.github.com/repos/$env:BUILD_REPOSITORY_ID/pulls/$env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
   $title = $response.title.ToLowerInvariant()
-  "##vso[task.setvariable variable=PullRequestTitle]$title"
-  Write-Host "Title: $title"
+  if ($title.Contains("[pack]")) {
+    $bypassPackaging = $false
+  }
 }
+
+# Write to output
+"##vso[task.setvariable variable=IncludeSuffix;isOutput=true]$includeSuffix"
+"##vso[task.setvariable variable=BypassPackaging;isOutput=true]$bypassPackaging"
