6389c1a352332475ef9dc684ca3e096eefc51112
Anatoli Beliaev <anatolib@microsoft.com>
2020-06-15 23:13:16 -0700

    Simplify FunctionInstanceLogger

--- src/WebJobs.Script.WebHost/Diagnostics/FunctionInstanceLogger.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/FunctionInstanceLogger.cs
@@ -19,12 +19,6 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
     // Adapter for capturing SDK events and logging them to tables.
     internal class FunctionInstanceLogger : IAsyncCollector<FunctionInstanceLogEntry>
     {
-        private const string Key = "metadata";
-
-        private readonly ILogWriter _writer;
-        private readonly IMetricsLogger _metrics;
-        private readonly IFunctionMetadataManager _metadataManager;
-
         public FunctionInstanceLogger(
             IFunctionMetadataManager metadataManager,
             IMetricsLogger metrics,
@@ -34,110 +28,24 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
             IDelegatingHandlerProvider delegatingHandlerProvider)
             : this(metadataManager, metrics)
         {
-            if (hostIdProvider == null)
-            {
-                throw new ArgumentNullException(nameof(hostIdProvider));
-            }
-
-            if (configuration == null)
-            {
-                throw new ArgumentNullException(nameof(configuration));
-            }
-
-            if (loggerFactory == null)
-            {
-                throw new ArgumentNullException(nameof(loggerFactory));
-            }
-
-            if (delegatingHandlerProvider == null)
-            {
-                throw new ArgumentNullException(nameof(delegatingHandlerProvider));
-            }
-
-            string accountConnectionString = configuration.GetWebJobsConnectionString(ConnectionStringNames.Dashboard);
-            if (accountConnectionString != null)
-            {
-                CloudStorageAccount account = CloudStorageAccount.Parse(accountConnectionString);
-                var restConfig = new RestExecutorConfiguration { DelegatingHandler = delegatingHandlerProvider.Create() };
-                var tableClientConfig = new TableClientConfiguration { RestExecutorConfiguration = restConfig };
-
-                var client = new CloudTableClient(account.TableStorageUri, account.Credentials, tableClientConfig);
-                var tableProvider = LogFactory.NewLogTableProvider(client);
-
-                ILogger logger = loggerFactory.CreateLogger(ScriptConstants.LogCategoryHostGeneral);
-
-                string hostId = hostIdProvider.GetHostIdAsync(CancellationToken.None).GetAwaiter().GetResult() ?? "default";
-                string containerName = Environment.MachineName;
-                _writer = LogFactory.NewWriter(hostId, containerName, tableProvider, (e) => OnException(e, logger));
-            }
         }
 
         internal FunctionInstanceLogger(IFunctionMetadataManager metadataManager, IMetricsLogger metrics)
         {
-            _metrics = metrics ?? throw new ArgumentNullException(nameof(metrics));
-            _metadataManager = metadataManager ?? throw new ArgumentNullException(nameof(metadataManager));
         }
 
-        public async Task AddAsync(FunctionInstanceLogEntry item, CancellationToken cancellationToken = default(CancellationToken))
+        public Task AddAsync(FunctionInstanceLogEntry item, CancellationToken cancellationToken = default(CancellationToken))
         {
-            FunctionInstanceMonitor monitor;
-            item.Properties.TryGetValue(Key, out monitor);
-
-            if (item.EndTime.HasValue)
-            {
-                // Function Completed
-                bool success = item.ErrorDetails == null;
-                monitor.End(success);
-            }
-            else
-            {
-                // Function Started
-                if (monitor == null)
-                {
-                    if (_metadataManager.TryGetFunctionMetadata(item.LogName, out FunctionMetadata function))
-                    {
-                        monitor = new FunctionInstanceMonitor(function, _metrics, item.FunctionInstanceId);
-                        item.Properties[Key] = monitor;
-                        monitor.Start();
-                    }
-                    else
-                    {
-                        // This exception will cause the function to not get executed.
-                        throw new InvalidOperationException($"Missing function.json for '{item.LogName}'.");
-                    }
-                }
-            }
-
-            if (_writer != null)
-            {
-                await _writer.AddAsync(new FunctionInstanceLogItem
-                {
-                    FunctionInstanceId = item.FunctionInstanceId,
-                    FunctionName = item.LogName,
-                    StartTime = item.StartTime,
-                    EndTime = item.EndTime,
-                    TriggerReason = item.TriggerReason,
-                    Arguments = item.Arguments,
-                    ErrorDetails = item.ErrorDetails,
-                    LogOutput = item.LogOutput,
-                    ParentId = item.ParentId
-                });
-            }
+            return Task.CompletedTask;
         }
 
         public Task FlushAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
-            if (_writer == null)
-            {
-                return Task.CompletedTask;
-            }
-
-            return _writer.FlushAsync();
+            return Task.CompletedTask;
         }
 
         public static void OnException(Exception exception, ILogger logger)
         {
-            logger.LogError($"Error writing logs to table storage: {exception.ToString()}", exception);
         }
     }
 }
