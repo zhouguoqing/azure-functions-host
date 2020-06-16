de4f427ea6951800ce085f29e504a71301340244
Anatoli Beliaev <anatolib@microsoft.com>
2020-06-15 23:46:13 -0700

    Remove perf data collection in FunctionExecutionFeature

--- src/WebJobs.Script.WebHost/Features/FunctionExecutionFeature.cs
+++ src/WebJobs.Script.WebHost/Features/FunctionExecutionFeature.cs
@@ -21,15 +21,11 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Features
     {
         private readonly IScriptJobHost _host;
         private readonly FunctionDescriptor _descriptor;
-        private readonly IEnvironment _environment;
-        private readonly ILogger _logger;
 
         public FunctionExecutionFeature(IScriptJobHost host, FunctionDescriptor descriptor, IEnvironment environment, ILoggerFactory loggerFactory)
         {
             _host = host ?? throw new ArgumentNullException(nameof(host));
             _descriptor = descriptor;
-            _environment = environment;
-            _logger = loggerFactory.CreateLogger(ScriptConstants.LogCategoryHostMetrics);
         }
 
         public bool CanExecute => _descriptor != null;
@@ -43,43 +39,11 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Features
                 throw new InvalidOperationException("Unable to execute function without a target.");
             }
 
-            JObject coldStartData = null;
-            if (request.IsColdStart())
-            {
-                coldStartData = new JObject
-                {
-                    { "requestId", request.GetRequestId() },
-                    { "language", Descriptor.Metadata.Language },
-                    { "sku", _environment.GetEnvironmentVariable(EnvironmentSettingNames.AzureWebsiteSku) }
-                };
-
-                var dispatchStopwatch = request.GetItemOrDefault<Stopwatch>(ScriptConstants.AzureFunctionsColdStartKey);
-                if (dispatchStopwatch != null)
-                {
-                    dispatchStopwatch.Stop();
-                    coldStartData.Add("dispatchDuration", dispatchStopwatch.ElapsedMilliseconds);
-                }
-            }
-
-            var sw = Stopwatch.StartNew();
             var arguments = new Dictionary<string, object>()
             {
                 { _descriptor.TriggerParameter.Name, request }
             };
             await _host.CallAsync(_descriptor.Name, arguments, cancellationToken);
-            sw.Stop();
-
-            if (coldStartData != null)
-            {
-                coldStartData.Add("functionDuration", sw.ElapsedMilliseconds);
-
-                var logData = new Dictionary<string, object>
-                {
-                    [ScriptConstants.LogPropertyEventNameKey] = ScriptConstants.ColdStartEventName,
-                    [ScriptConstants.LogPropertyActivityIdKey] = request.GetRequestId()
-                };
-                _logger.Log(LogLevel.Information, 0, logData, null, (s, e) => coldStartData.ToString(Formatting.None));
-            }
         }
     }
 }
