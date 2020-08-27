162ac47132aa876bc0574219b5779565b347d3b8
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-27 16:40:13 -0700

    Make run-crank-agent.ps1 work on both Windows and Linux

---
+++ tools/Crank/run-crank-agent.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'Stop'
+
+if ($IsWindows) {
+    Set-MpPreference -DisableRealtimeMonitoring $true
+    Add-MpPreference -ExclusionProcess 'Microsoft.Azure.WebJobs.Script.WebHost.exe'
+    Add-MpPreference -ExclusionProcess 'crank-agent.exe'
+}
+
+$logsDir = $IsWindows ? 'C:\crank-agent-logs' : '/home/Functions/crank-agent-logs'
+if (-not (Test-Path $logsDir -PathType Container)) {
+    New-Item -Path $logsDir -ItemType Container
+}
+
+$logFileName = Join-Path -Path $logsDir -ChildPath "$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').log"
+
+$invokeCrankAgentCommand = $IsWindows ? 'C:\dotnet-tools\crank-agent.exe' : '/home/Functions/.dotnet/tools/crank-agent';
+
+& $invokeCrankAgentCommand 2>&1 >> $logFileName
