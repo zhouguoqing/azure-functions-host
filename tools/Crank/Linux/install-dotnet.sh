f3baf924dc9067d87811cba20b091d6e427f3b5f
Anatoli Beliaev <anatolib@microsoft.com>
2020-08-28 01:04:41 -0700

    Remove unnecessary sh scripts

--- tools/Crank/Linux/install-dotnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# From https://docs.microsoft.com/dotnet/core/install/linux-ubuntu#install-the-sdk
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-3.1
--- tools/Crank/Linux/setup-crank-agent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dotnet tool install -g Microsoft.Crank.Agent --version "0.1.0-*"
-
-line="@reboot /home/Functions/.dotnet/tools/crank-agent"
-(crontab -l; echo "$line" ) | crontab -
